0263f045f3cfff331e94a3bcca3f1d117fa23fe3
Sang Han <VinTeK1@gmail.com>
2015-07-14 17:13:57 -0400

    add additional argument parsing capabilities
    - switch between encryption/decryption mode
    - use either a string from shell or filename for the message
    - use either a key string or key length

--- vigenere.py
+++ vigenere.py
@@ -17,15 +17,15 @@ def getPosFromIndex(xss, index):
             r = i
     return r, index - sum(map(len, xss[:r]))
 
-def getIndexOfKey(key, ciphertext, index):
-    """ Return the index in this key being used to decipher ciphertext[index].
+def getIndexOfKey(key, text, index):
+    """ Return the index in this key being used to decipher text[index].
 
-    This is necessary since the ciphertext may include non-ascii chars which
+    This is necessary since the text may include non-ascii chars which
     the key must skip over.
     """
     keyIndex, i = 0, 0
     while i != index:
-        if ciphertext[i].isalpha():
+        if text[i].isalpha():
             keyIndex = (keyIndex + 1) % len(key)
         i += 1
     return keyIndex
@@ -74,20 +74,20 @@ def decipher(ciphertext, key):
         ret.append(p)
     return "".join(ret)
 
-def printMessage(window, ciphertext, key, index, highlight):
+def printMessage(window, text, key, index, highlight):
     """ Print the current message as deciphered by this key.
 
     Args:
         window (curses.window): curses window to render onto.
-        ciphertext ([str]): ciphertext message.
+        text ([str]): message to encrypt or decrypt.
         key ([str]): key used for deciphering.
-        index (int): current highlighted character in ciphertext.
+        index (int): current highlighted character in message.
         highlight (bool): should highlighting should be performed?
     """
     h, w = window.getmaxyx()
     # This was really tricky to debug: the total length of a message before
     # and after it is textwrapped MAY NOT be equal since spaces may be removed.
-    msg = decipher(ciphertext, key)
+    msg = cipher(text, key)
     wrapped = textwrap.TextWrapper(drop_whitespace=False, width=w-4).wrap(msg)
     r, c = getPosFromIndex(wrapped, index)
 
@@ -116,42 +116,71 @@ def printKey(window, key, index, highlight):
 
 ####ARG_PARSING################################################################
 
-if len(sys.argv) < 3:
-    print('usage: '+sys.argv[0]+' text|file.txt key_length')
+if len(sys.argv) != 5:
+    print('usage: '+sys.argv[0]+
+            ' -e|-d text|file.txt -k key_string|-l key_length')
     sys.exit(-1)
 
-try:
-    fileName, keyLen = sys.argv[1], int(sys.argv[2])
-except ValueError:
-    print('not a valid key length:', sys.argv[2])
+# Parse cipher mode.
+if sys.argv[1] == '-e':
+    cipher = encipher
+elif sys.argv[1] == '-d':
+    cipher = decipher
+else:
+    print('use either -e|-d for enciphering or deciphering')
     sys.exit(-1)
 
+# Parse filename or message.
 try:
-    with open(fileName) as f:
-        ciphertext = [ch for ch in f.read().strip()]
+    with open(sys.argv[2]) as f:
+        text = [ch for ch in f.read().strip()]
+# If it isn't openable as a file, then use it as the message itself.
 except FileNotFoundError:
-    ciphertext = [ch for ch in sys.argv[1].strip()]
+    text = [ch for ch in sys.argv[1].strip()]
+
+# Parse key mode.
+if sys.argv[3] == '-k':
+    keyLenMode = False
+elif sys.argv[3] == '-l':
+    keyLenMode = True
+else:
+    print('use either -k key_string|-l key_length')
+    sys.exit(-1)
+
+# Parse key string or length.
+try:
+    if keyLenMode:
+        keyLen = int(sys.argv[4])
+        key = ['A']*keyLen
+    else:
+        # If the key is directly provided, just print out the result.
+        print(cipher(text, sys.argv[4]))
+        sys.exit(0)
+except ValueError:
+    print('not a valid key length:', sys.argv[4])
+    sys.exit(-1)
 
 ####MAIN#######################################################################
 
 def main(stdscr):
     index = 0
     keyMode = True
-    key = ['A']*keyLen
-    message = key if keyMode else ciphertext
+    message = key if keyMode else text
 
     curses.curs_set(0) # make the cursor invisible
 
     while True:
         # Render calls.
         stdscr.clear()
-        printMessage(stdscr, ciphertext, key, index, not keyMode)
+        printMessage(stdscr, text, key, index, not keyMode)
         printKey(stdscr, key, index, keyMode)
         stdscr.refresh()
 
         # Input parsing.
         ch = stdscr.getch()
         if ch == 27: # ESCAPE key
+            curses.endwin()
+            print(cipher(text, key))
             break
         elif ch == curses.KEY_DOWN and keyMode:
             key[index] = offsetChar(key[index], -1)
@@ -165,15 +194,15 @@ def main(stdscr):
         elif ch == ord(' '):
             keyMode = not keyMode
             index = 0
-            message = key if keyMode else ciphertext
+            message = key if keyMode else text
         elif ch in list(map(ord, string.ascii_letters)):
             # Set key to arbitrary character. Message is changed.
             if keyMode:
                 key[index] = str.upper(chr(ch))
             # Set message to arbitrary character. Key is changed.
             else:
-                keyIndex = getIndexOfKey(key, ciphertext, index)
-                key[keyIndex] = offsetChar(ciphertext[index], ord('a')-ch)
+                keyIndex = getIndexOfKey(key, text, index)
+                key[keyIndex] = offsetChar(text[index], ord('a')-ch)
             index = offsetIndex(message, index, True)
 
 wrapper(main)
